4ea081f8eb7a44ce53e0ee4c35de6a3feff9d513
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-10-19 17:06:14 +0300

    The added new year app

---
+++ greetings/newyear/views.py
@@ -0,0 +1,14 @@
+import datetime
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html",{
+        "newyear": now.month == 1 and now.day ==1,
+        "month": now.month,
+        "day": now.days,
+        "year": now.year 
+    })
+
