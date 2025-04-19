36db0c03890e39f7af359a14b12759ae7e915dc7
Stedoh <stevensimoni55@gmail.com>
2025-04-19 17:48:46 +0300

    Adding the form for submitting comments

--- news/articles/admin.py
+++ news/articles/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 # Register your models here.
+class CommentInline(admin.TabularInline): # new
+    model = Comment 
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["id","author", "date", "title", "body",]
+    inlines = [CommentInline]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id","date", "author", "article", "comment"]
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment,CommentAdmin)
