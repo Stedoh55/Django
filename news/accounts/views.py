6a307979dbc9f1f1f585772ba4b8fbf76e58145e
Stedoh <stevensimoni55@gmail.com>
2025-04-19 18:11:11 +0300

    Implementing the logout view

--- news/accounts/views.py
+++ news/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -9,3 +10,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('home')
--- news/django_project/urls.py
+++ news/django_project/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('', include('accounts.urls')),
     path("", include("pages.urls")),
     path("articles/", include("articles.urls")),
 ]
